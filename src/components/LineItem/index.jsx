9b502e4f5aeeefbfc4600252bad795c3e24d1405
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-08-21 03:40:34 -0500

    Add LineItem and LineItems component structures

---
+++ src/components/LineItem/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import {MdCancel as DeleteIcon} from 'react-icons/md'
+
+import styles from './LineItem.module.scss'
+
+
+class LineItem extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default LineItem;
