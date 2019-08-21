6428786bd61b02f432240811d3e1d4cfb0ef83d3
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-08-21 03:47:01 -0500

    add dnd

--- src/components/LineItems/index.jsx
+++ src/components/LineItems/index.jsx
@@ -1,2 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
 import LineItem from './LineItem'
+
+import { MdAddCircle as AddIcon } from 'react-icons/md'
+import styles from './LineItems.module.scss'
+
+const LineItems = ({items}) => {
+    return (  );
+}
+ 
+export default LineItems;
+
+
