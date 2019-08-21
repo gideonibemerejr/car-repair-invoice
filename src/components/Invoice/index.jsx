789805143b3b6bc1c4e69c990630e76a539c43e5
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-08-21 15:27:08 -0500

    Update Invoice Header to include Billed To addressee

--- src/components/Invoice/index.jsx
+++ src/components/Invoice/index.jsx
@@ -11,9 +11,10 @@ class Invoice extends Component {
     state = { 
         date: new Date().toLocaleDateString() ,
         taxRate: 0.00,
+        labor: 0,
         lineItems: [
             {   
-                
+                id: 'initial',
                 item: '',
                 description: '',
                 quantity: 0,
@@ -22,6 +23,17 @@ class Invoice extends Component {
             },
         ]
      }
+     handleInvoiceChange = (e) => {
+       this.setState({[e.target.name]: e.target.value})
+     }
+     handleLineItemChange = (elementIndex) => (event) => {
+       let lineItems = this.state.lineItems.map((item, i) => {
+         if (elementIndex !== i) return item
+         return {...item, [event.target.name]: event.target.value }
+       })
+       this.setState({lineItems})
+     }
+    
     render() { 
         return ( 
             <div className={styles.invoice}>
@@ -30,7 +42,7 @@ class Invoice extends Component {
             </div> */}
             <div className={styles.addresses}>
               <div className={styles.from}>
-                <strong>Wagner Automotive</strong><br />
+                <strong>Professional Automotive</strong><br />
                   123 Kensington Ave<br />
                   Toronto, ON, Canada &nbsp;A1B2C3<br />
                   416-555-1234
@@ -52,6 +64,18 @@ class Invoice extends Component {
                 </div>
               </div>
             </div>
+            <div className="flex justify-between">
+              <div className="mt4 mr5 tl">
+                <strong>Billed To</strong>
+              </div>
+              <div className="mt4 tr">
+              <strong>James Walker</strong><br />
+                  123 Sesame Street<br />
+                  Albany, NY &nbsp;01252<br />
+                  416-555-1234
+              </div>
+                
+              </div>
             <h2>Invoice</h2>
             <LineItems
             items={this.state.lineItems}
@@ -70,6 +94,10 @@ class Invoice extends Component {
                 <div className={styles.value}>
                     <input name="taxRate" type="number" step="0.01" value={this.state.taxRate} onChange={this.handleInvoiceChange} onFocus={this.handleFocusSelect} />
                 </div>
+                <div className={styles.label}>Labor Hours</div>
+                <div className={styles.value}>
+                    <input name="labor" type="number" step="0.01" value={this.state.labor} onChange={this.handleInvoiceChange} onFocus={this.handleFocusSelect} />
+                </div>
               </div>
             </div>
           </form>
@@ -80,9 +108,14 @@ class Invoice extends Component {
                 {/* <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcLineItemsTotal())}</div> */}
               </div>
               <div className={styles.row}>
+                <div className={styles.label}>Labor ({this.state.labor} hrs)</div>
+                {/* <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcGrandTotal())}</div> */}
+              </div>
+              <div className={styles.row}>
                 <div className={styles.label}>Tax ({this.state.taxRate}%)</div>
                 {/* <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcTaxTotal())}</div> */}
               </div>
+              
               <div className={styles.row}>
                 <div className={styles.label}>Total Due</div>
                 {/* <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcGrandTotal())}</div> */}
