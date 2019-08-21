85d7a04c49153a3b2bf9f8e985fa797acc69d955
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-08-21 03:37:06 -0500

    Add main Invoice without line item components

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.module.scss';
+import Invoice from './Invoice'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { 
+    invoices: 0
+   }
+  render() { 
+    return ( 
+      <div className="App">
+        <Invoice numberOfInvoices={this.state.invoices + 1}/>
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
+
---
+++ src/components/Invoice/index.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import styles from './Invoice.module.scss'
+
+class Invoice extends Component {
+    
+    local = 'en-US'
+    currency = 'USD'
+
+    state = { 
+        date: new Date().toLocaleDateString() ,
+        taxRate: 0.00,
+        lineItems: [
+            {   
+                
+                item: '',
+                description: '',
+                quantity: 0,
+                price: 0.00,
+
+            },
+        ]
+     }
+    render() { 
+        return ( 
+            <div className={styles.invoice}>
+            {/* <div className={styles.brand}>
+              <img src="https://via.placeholder.com/150x50.png?text=logo" alt="Logo" className={styles.logo} />
+            </div> */}
+            <div className={styles.addresses}>
+              <div className={styles.from}>
+                <strong>Wagner Automotive</strong><br />
+                  123 Kensington Ave<br />
+                  Toronto, ON, Canada &nbsp;A1B2C3<br />
+                  416-555-1234
+              </div>
+              <div>
+                <div className={`${styles.valueTable} ${styles.to}`}>
+                  <div className={styles.row}>
+                    <div className={styles.label}>Customer #</div>
+                    <div className={styles.value}>123456</div>
+                  </div>
+                  <div className={styles.row}>
+                    <div className={styles.label}>Invoice #</div>
+                    <div className={styles.value}>0000{this.props.numberOfInvoices}</div>
+                  </div>
+                  <div className={styles.row}>
+                    <div className={styles.label}>Date</div>
+                    <div className={`${styles.value} ${styles.date}`}>{this.state.date}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <h2>Invoice</h2>
+            {/* <LineItems /> */}
+            <div className={styles.totalContainer}>
+          <form>
+            <div className={styles.valueTable}>
+              <div className={styles.row}>
+                <div className={styles.label}>Tax Rate (%)</div>
+                <div className={styles.value}>
+                    <input name="taxRate" type="number" step="0.01" value={this.state.taxRate} onChange={this.handleInvoiceChange} onFocus={this.handleFocusSelect} />
+                </div>
+              </div>
+            </div>
+          </form>
+          <form>
+            <div className={styles.valueTable}>
+              <div className={styles.row}>
+                <div className={styles.label}>Subtotal</div>
+                {/* <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcLineItemsTotal())}</div> */}
+              </div>
+              <div className={styles.row}>
+                <div className={styles.label}>Tax ({this.state.taxRate}%)</div>
+                {/* <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcTaxTotal())}</div> */}
+              </div>
+              <div className={styles.row}>
+                <div className={styles.label}>Total Due</div>
+                {/* <div className={`${styles.value} ${styles.currency}`}>{this.formatCurrency(this.calcGrandTotal())}</div> */}
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div className={styles.pay}>
+          <button className={styles.payNow} onClick={this.handlePayButtonClick}>Pay Now</button>
+        </div>
+            </div>
+         );
+    }
+}
+ 
+export default Invoice;
